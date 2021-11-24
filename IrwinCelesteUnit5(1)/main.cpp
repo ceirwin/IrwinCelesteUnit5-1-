4b39ac9779b24b4a6491e6084f6cd3ac03e3ef24
Celeste Irwin <celeste.irwin@sinclair.edu>
2021-11-24 14:37:57 -0500

    Initial Commit

---
+++ IrwinCelesteUnit5(1)/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  IrwinCelesteUnit5(1)
+//
+//  Created by Celeste Irwin on 11/24/21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Hello, World!\n";
+    return 0;
+}
